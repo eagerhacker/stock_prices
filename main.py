02ecc7282afe338fa10dc77d27a9de8c72005211
bthisstriving <bthanhtrx@gmail.com>
2024-03-01 18:23:02 +0700

    Add tab control.

--- main.py
+++ main.py
@@ -18,42 +18,49 @@ def get_stock_data(stock_market_name, startDate='', endDate='', page_index = 1) 
 
 st.set_page_config(layout='wide', page_title='StreamlitApp')
 
-main_page = st.container()
-cols = st.columns(8)
+tabs = st.tabs(['Table', 'Chart'])
+with tabs[0]:
+    main_page = st.container()
+    cols = st.columns(8)
 
-if('pageIndex' not in st.session_state):
-    st.session_state.pageIndex = 1
+    if('pageIndex' not in st.session_state):
+        st.session_state.pageIndex = 1
 
-with st.sidebar:
-    data = []
-    with st.form('my_form'):
+    with st.sidebar:
+        data = []
+        with st.form('my_form'):
 
-        name = st.text_input('Stock market code')
-        start_date = st.date_input('Start').strftime("%m/%d/%Y")
-        end_date = st.date_input('End').strftime("%m/%d/%Y")
-        
-        # print(start_date.strftime("%m/%d/%Y"))
-        # print(end_date.strftime("%d/%m/%Y"))
-        
+            name = st.text_input('Stock market code')
+            start_date = st.date_input('Start').strftime("%m/%d/%Y")
+            end_date = st.date_input('End').strftime("%m/%d/%Y")
+            
+            # print(start_date.strftime("%m/%d/%Y"))
+            # print(end_date.strftime("%d/%m/%Y"))
+            
 
-        submit = st.form_submit_button('Submit')
-        if(submit):
-            data = get_stock_data(name, startDate=start_date, endDate=end_date)
-            st.session_state.pageIndex = 1
+            submit = st.form_submit_button('Submit')
+            if(submit):
+                data = get_stock_data(name, startDate=start_date, endDate=end_date)
+                st.session_state.pageIndex = 1
 
-    with cols[0]:
-        if(st.session_state.pageIndex > 1):        
-            if(st.button('Prev')):
-                st.session_state.pageIndex -= 1
+        with cols[0]:
+            if(st.session_state.pageIndex > 1):        
+                if(st.button('Prev')):
+                    st.session_state.pageIndex -= 1
+                    data = get_stock_data(name, st.session_state.pageIndex)
+
+        with cols[7]:
+            if(st.button('Next')):
+                st.session_state.pageIndex += 1
                 data = get_stock_data(name, st.session_state.pageIndex)
 
-    with cols[7]:
-        if(st.button('Next')):
-            st.session_state.pageIndex += 1
-            data = get_stock_data(name, st.session_state.pageIndex)
+        with main_page:
+            if(len(data) > 0):
+                st.table(pd.DataFrame(data))        
 
-    with main_page:
-        if(len(data) > 0):
-            st.table(pd.DataFrame(data))        
+        
+with tabs[1]:
+    df = pd.DataFrame(data)
+    st.bar_chart(x='Ngay', y='GiaDongCua', data=df)
 
-    
+    st.line_chart(x='Ngay', y='GiaDongCua', data=df)
