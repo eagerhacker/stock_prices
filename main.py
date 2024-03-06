4f1f745b6278a16a2565f7a8e0ebaa52b669eea1
bthisstriving <bthanhtrx@gmail.com>
2024-03-06 18:04:26 +0700

    Add checkbox for optional date range query.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime 
 
-def get_stock_data(stock_market_name, startDate='', endDate='', page_index = 1) -> dict:
+def get_stock_data(stock_market_name, startDate='', endDate='', page_index = 1):
     url = f'https://s.cafef.vn/Ajax/PageNew/DataHistory/PriceHistory.ashx?Symbol={stock_market_name}&StartDate={startDate}&EndDate={endDate}&PageIndex={page_index}'
 
     print(url)
@@ -18,31 +18,34 @@ def get_stock_data(stock_market_name, startDate='', endDate='', page_index = 1) 
 
 st.set_page_config(layout='wide', page_title='StreamlitApp')
 
+with st.sidebar:
+    data = []
+    
+    name = st.text_input('Stock market code')
+    start_date, end_date = '', ''
+
+    with_date = st.checkbox('Include daterange')
+    if with_date:
+        start_date = st.date_input('Start').strftime("%m/%d/%Y")
+        end_date = st.date_input('End').strftime("%m/%d/%Y")
+    
+    # print(start_date.strftime("%m/%d/%Y"))
+    # print(end_date.strftime("%d/%m/%Y"))
+        
+    submit = st.button('Submit')
+    if(submit):
+        data = get_stock_data(name, startDate=start_date, endDate=end_date)
+        st.session_state.pageIndex = 1
+
+st.title(name.upper())
+
 tabs = st.tabs(['Table', 'Chart'])
 with tabs[0]:
     main_page = st.container()
     cols = st.columns(8)
 
-    if('pageIndex' not in st.session_state):
-        st.session_state.pageIndex = 1
-
-    with st.sidebar:
-        data = []
-        with st.form('my_form'):
-
-            name = st.text_input('Stock market code')
-            start_date = st.date_input('Start').strftime("%m/%d/%Y")
-            end_date = st.date_input('End').strftime("%m/%d/%Y")
-            
-            # print(start_date.strftime("%m/%d/%Y"))
-            # print(end_date.strftime("%d/%m/%Y"))
-            
-
-            submit = st.form_submit_button('Submit')
-            if(submit):
-                data = get_stock_data(name, startDate=start_date, endDate=end_date)
-                st.session_state.pageIndex = 1
-
+    if(len(data) > 0):
+    
         with cols[0]:
             if(st.session_state.pageIndex > 1):        
                 if(st.button('Prev')):
@@ -57,10 +60,11 @@ with tabs[0]:
         with main_page:
             if(len(data) > 0):
                 st.table(pd.DataFrame(data))        
-
-        
+            
 with tabs[1]:
-    df = pd.DataFrame(data)
-    st.bar_chart(x='Ngay', y='GiaDongCua', data=df)
+    if(len(data) > 0):
+        df = pd.DataFrame(data)
+        bar = st.bar_chart(x='Ngay', y='GiaDongCua', data=df)
+        bar.lab
 
-    st.line_chart(x='Ngay', y='GiaDongCua', data=df)
+        st.line_chart(x='Ngay', y='GiaDongCua', data=df)
